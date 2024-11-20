9c34918b0c21a7f6599425af7ef917d0995008e4
Martaa04 <147490812+Martaa04@users.noreply.github.com>
2024-11-20 13:34:31 +0100

    log in works

--- SO_Game/Main.Designer.cs
+++ SO_Game/Main.Designer.cs
@@ -50,6 +50,7 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label4 = new System.Windows.Forms.Label();
             this.label_users_connected = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -258,11 +259,22 @@
             this.label_users_connected.TabIndex = 21;
             this.label_users_connected.Text = "0";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(868, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Show Connected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label_users_connected);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
@@ -319,5 +331,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label_users_connected;
+        private System.Windows.Forms.Button button1;
     }
 }
--- SO_Game/Main.cs
+++ SO_Game/Main.cs
@@ -48,7 +48,7 @@ namespace SO_Game
         private void connect_Click(object sender, EventArgs e)
         {
             IPAddress direc = IPAddress.Parse("192.168.56.102");
-            IPEndPoint ipep = new IPEndPoint(direc, 50168);
+            IPEndPoint ipep = new IPEndPoint(direc, 50182);
 
 
             //We create the socket
@@ -106,6 +106,40 @@ namespace SO_Game
 
                     byte[] answer = new byte[1024];
                     server.Receive(answer);
+
+                    switch (Encoding.ASCII.GetString(answer))
+                    {
+                        case "0":
+
+                            MessageBox.Show("The email is already registered. Use another one.");
+
+                            break;
+
+                        case "1":
+                            MessageBox.Show("This username already exists. Please, choose another one.");
+                            break;
+
+                        case "2":
+                            MessageBox.Show("The email must have between 15 and 80 characters.");
+                            break;
+
+                        case "3":
+                            MessageBox.Show("Your username must have between 3 and 80 characters.");
+                            break;
+
+                        case "4":
+                            MessageBox.Show("Your passwrd must have between 8 and 20 characters");
+                            break;
+
+                        case "5":
+                            MessageBox.Show("Register was unsuccessful. Please, try again.");
+                            break;
+
+                        case "6":
+                            MessageBox.Show("New user registered successfully!");
+                            this.Hide();
+                            break;
+                    }
                 }
             }
 
@@ -115,12 +149,94 @@ namespace SO_Game
             }
         }
 
+        private void query_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = ""; // Prepare the message to send to the server
+
+                if (PlayerGame.Checked)
+                {
+                    message = "3/";
+                    // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+                    server.Send(msg);
+
+                    byte[] msg2 = new byte[512];
+                    server.Receive(msg2);
+                    string mensaje = Encoding.ASCII.GetString(msg2);
+
+                    string formattedMessage = mensaje.Replace(",", "\n");
+                    MessageBox.Show(formattedMessage);
+
+                }
+                else if (winner.Checked)
+                {
+                    message = "4/" + gameid.Text; // The code for fetching the winner by game ID
+                                                  // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+                    server.Send(msg);
+                    byte[] msg2 = new byte[512];
+                    server.Receive(msg2);
+                    string mensaje = Encoding.ASCII.GetString(msg2);
+                    MessageBox.Show(mensaje);
+                }
+                else if (gamesPlayed.Checked)  // New case for Games Played by a Player
+                {
+                    message = "6/" + playerName.Text; // "6" represents the new query
+                                                      // Enviamos al servidor el nombre tecleado                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+                    server.Send(msg);
+                    byte[] msg2 = new byte[512];
+                    server.Receive(msg2);
+                    string mensaje = Encoding.ASCII.GetString(msg2);
+                    MessageBox.Show(message);
+                }
+            }
+            catch (SocketException ex)
+            {
+                // If there's a socket exception, show an error message
+                MessageBox.Show("Error connecting to the server: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensaje = "5/";
+            byte[] msg = Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            byte[] msg2 = new byte[1024];
+            server.Receive(msg2);
+            string message = Encoding.ASCII.GetString(msg2);
+            int m = Convert.ToInt32(message.Split(',')[0]);
+
+            label_users_connected.Text = "Users connected:";
+            label_users_connected.Text = label_users_connected.Text + " " + Convert.ToString(m);
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear(); // Clear columns first
+            dataGridView1.Columns.Add("PlayerName", "Connected Players");
+
+
+            string name;
+
+            for (int i = 0; i < m; i++)
+            {
+                name = Convert.ToString(Encoding.ASCII.GetString(msg2).Split(',')[i]);
+
+
+                dataGridView1.Rows.Add(name);
+
+            }
+        }
+
         private void AtenderServidor() //receive ALL the messages from the server!!
         {
             while (true)
             {
                 //Recibimos mensaje del servidor
-                byte[] msg2 = new byte[80];
+                byte[] msg2 = new byte[512];
                 server.Receive(msg2);
                 string[] trozos = Encoding.ASCII.GetString(msg2).Split('/');
                 string codigo = (trozos[0]);
@@ -201,24 +317,27 @@ namespace SO_Game
                         break;
 
                     case "6":
-                        switch (mensaje)
+                        switch (Convert.ToInt32(mensaje))
                         {
-                            case "0":
+                            case 0:
 
                                 MessageBox.Show("The userame does not exist.");
                                 break;
 
-                            case "1":
+                            case 1:
                                 MessageBox.Show("Login error. Please, try again.");
                                 break;
 
-                            case "2":
+                            case 2:
                                 MessageBox.Show("The password is not correct.");
                                 break;
 
-                            case "3":
+                            case 3:
 
                                 MessageBox.Show("Login successful");
+                                break;
+                            default:
+                                MessageBox.Show(Convert.ToString(mensaje));
                                 break;
                         }
                         break;
@@ -227,40 +346,5 @@ namespace SO_Game
             }
         }
 
-        private void query_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                string message = ""; // Prepare the message to send to the server
-
-                if (PlayerGame.Checked)
-                {
-                    message = "3/";
-                    // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
-                    server.Send(msg);
-
-                }
-                else if (winner.Checked)
-                {
-                    message = "4/" + gameid.Text; // The code for fetching the winner by game ID
-                                                  // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
-                    server.Send(msg);
-                }
-                else if (gamesPlayed.Checked)  // New case for Games Played by a Player
-                {
-                    message = "6/" + playerName.Text; // "6" represents the new query
-                                                      // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
-                    server.Send(msg);
-                }
-            }
-            catch (SocketException ex)
-            {
-                // If there's a socket exception, show an error message
-                MessageBox.Show("Error connecting to the server: " + ex.Message);
-            }
-        }
     }
 }
